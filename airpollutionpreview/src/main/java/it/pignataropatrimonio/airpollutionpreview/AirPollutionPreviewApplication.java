d03e08d83c8e484fddffe6bff83e3a0d6cdba6c0
pricciardi <pietro.ricciardi@gmail.com>
2019-07-04 16:44:21 +0200

    Componente di Front End
    Struttura programma per la visualizzazione dei dati dei sensori

---
+++ airpollutionpreview/src/main/java/it/pignataropatrimonio/airpollutionpreview/AirPollutionPreviewApplication.java
@@ -0,0 +1,13 @@
+package it.pignataropatrimonio.airpollutionpreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPollutionPreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirPollutionPreviewApplication.class, args);
+	}
+
+}
---
+++ airpollutionpreview/src/test/java/it/pignataropatrimonio/airpollutionpreview/AirPollutionPreviewApplicationTests.java
@@ -0,0 +1,16 @@
+package it.pignataropatrimonio.airpollutionpreview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirPollutionPreviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
